98fcd29e750f1b715ec571f65643c6b8d3d082b2
Renan Augusto Romig <renanromig@hotmail.com>
2025-03-20 14:48:18 -0300

    change unit test to triple a and go testify

--- internal/domain/campaign/campaign_test.go
+++ internal/domain/campaign/campaign_test.go
@@ -1,21 +1,25 @@
 package campaign
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestNewcampaign(t *testing.T) {
+	//arrange
+	assert := assert.New(t)
 	name := "Campaign X"
 	content := "Body"
 	contacts := []string{"email1@e.com", "email2@e.com"}
+
+	//act
 	campaign := NewCampaign(name, content, contacts)
 
-	if campaign.Id != "1" {
-		t.Error("Expected 1", campaign.Id)
-	} else if campaign.Name != name {
-		t.Error("Expected correct name", campaign.Name)
-	} else if campaign.Content != content {
-		t.Error("Expected correct content", campaign.Content)
-	} else if len(campaign.Contacts) != len(contacts) {
-		t.Error("Expected correct contacts", campaign.Contacts)
-	}
+	//assert
+	assert.Equal(campaign.Id, "1")
+	assert.Equal(campaign.Name, name)
+	assert.Equal(campaign.Content, content)
+	assert.Equal(len(campaign.Contacts), len(contacts))
 
 }
