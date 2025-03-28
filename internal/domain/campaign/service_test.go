117852e864dae69a6f2ec9489bac17a7a4c3256d
Renan Augusto Romig <renanromig@hotmail.com>
2025-03-28 10:53:00 -0300

    ad tests by getby func

--- internal/domain/campaign/service_test.go
+++ internal/domain/campaign/service_test.go
@@ -25,8 +25,11 @@ func (r *repositoryMock) Get() ([]Campaign, error) {
 }
 
 func (r *repositoryMock) GetBy(id string) (*Campaign, error) {
-	//args := r.Called(campaign)
-	return nil, nil
+	args := r.Called(id)
+	if args.Error(1) != nil {
+		return nil, args.Error(1)
+	}
+	return args.Get(0).(*Campaign), nil
 }
 
 var (
@@ -75,4 +78,33 @@ func Test_Create_ValidateRepositorySave(t *testing.T) {
 	_, err := service.Create(newCampaign)
 
 	assert.True(errors.Is(internalerrors.ErrInternal, err))
+}
+
+func Test_GetById_ReturnCampaign(t *testing.T) {
+	assert := assert.New(t)
+	campaign, _ := NewCampaign(newCampaign.Name, newCampaign.Content, newCampaign.Emails)
+	repositoryMock := new(repositoryMock)
+	repositoryMock.On("GetBy", mock.MatchedBy(func(id string) bool {
+		return id == campaign.Id
+	})).Return(campaign, nil)
+	service.Repository = repositoryMock
+
+	campaignReturned, _ := service.GetBy(campaign.Id)
+
+	assert.Equal(campaign.Id, campaignReturned.Id)
+	assert.Equal(campaign.Name, campaignReturned.Name)
+	assert.Equal(campaign.Content, campaignReturned.Content)
+	assert.Equal(campaign.Status, campaignReturned.Status)
+}
+
+func Test_GetById_ReturnErrorWhenSomethingWrongExist(t *testing.T) {
+	assert := assert.New(t)
+	campaign, _ := NewCampaign(newCampaign.Name, newCampaign.Content, newCampaign.Emails)
+	repositoryMock := new(repositoryMock)
+	repositoryMock.On("GetBy", mock.Anything).Return(nil, errors.New("Something wrong"))
+	service.Repository = repositoryMock
+
+	_, err := service.GetBy(campaign.Id)
+
+	assert.Equal(err.Error(), internalerrors.ErrInternal.Error())
 }
